1afaa8532cdc4d1eb60b341c113f10f7b009ec33
FranklinOmar23 <dislafrankli52@gmail.com>
2024-02-13 21:14:40 -0400

    creacion de animacion dellogin y registro

--- front_end/src/Componentes/login.jsx
+++ front_end/src/Componentes/login.jsx
@@ -5,6 +5,7 @@ import '../Css/Footer-Dark-icons.css';
 import '../Css/sidebar-menu.css';
 import '../Css/animate.min.css';
 import '../Css/loginestilo.css';
+import '../Css/adicciones.css';
 import axios from 'axios';
 import toast, { Toaster } from 'react-hot-toast';
 
@@ -29,43 +30,47 @@ const Login = () => {
 
   return (
     <div className="login-background">
-      <Toaster 
-        toastOptions={{
-          style: {
-            height: '90px',
-            width: '300px',
-            fontSize: '16px',
-          },
-        }}
-      />
-      <div className="container pulse animated">
-        <div className="row justify-content-center">
-          <div className="col-md-9 col-lg-12 col-xl-10">
-            <div className="card shadow-lg o-hidden border-0 my-5">
-              <div className="card-body p-0">
-                <div className="row">
-                  <div className="col-lg-6 d-none d-lg-flex">
-                    <img
-                      className="img-fluid m-auto"
-                      src="https://nizaero.com/wp-content/uploads/2018/07/Logo-DIGESETT-292x300.png"
-                      alt="Logo"
-                    />
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="p-5" style={{ background: 'var(--bs-card-border-color)' }}>
-                      <div className="text-center">
-                        <h4 className="text-dark mb-4">¡Bienvenido!</h4>
-                      </div>
-                      <LoginCampos onLogin={handleLogin} />
-                      <div className="text-center">
-                        <Link to="/olvidoClave" className="link-body-emphasis small">
-                          ¿Olvidaste tu contraseña?
-                        </Link>
-                      </div>
-                      <div className="text-center">
-                        <Link to="/registro" className="link-body-emphasis small">
-                          ¡Crea tu cuenta!
-                        </Link>
+      <div class="bubbles">
+        <Toaster
+          toastOptions={{
+            style: {
+              height: '90px',
+              width: '300px',
+              fontSize: '16px',
+            },
+          }}
+        />
+        <div className="container1 pulse animated">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-md-9 col-lg-12 col-xl-10">
+              <div className="card shadow-lg o-hidden border-0 my-5">
+                <div className="card-body p-0">
+                  <div className="row container3">
+                    <div className="col-lg-6 d-none d-lg-flex flex-column align-items-center">
+                      <img
+                        className="img-fluid m-auto justify-content-t"
+                        src="https://nizaero.com/wp-content/uploads/2018/07/Logo-DIGESETT-292x300.png"
+                        alt="Logo"
+                      />
+                      <h2 >DIGESETT</h2>
+                      <h3 className="text-center">Sistema de gestion de transito</h3>
+                    </div>
+                    <div className="col-lg-6">
+                      <div className="p-5" style={{ background: 'var(--bs-card-border-color)' }}>
+                        <div className="text-center">
+                          <h4 className="text-dark mb-4">¡Bienvenido!</h4>
+                        </div>
+                        <LoginCampos onLogin={handleLogin} />
+                        <div className="text-center">
+                          <Link to="/olvidoClave" className="link-body-emphasis small">
+                            ¿Olvidaste tu contraseña?
+                          </Link>
+                        </div>
+                        <div className="text-center">
+                          <Link to="/registro" className="link-body-emphasis small">
+                            ¡Crea tu cuenta!
+                          </Link>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -74,8 +79,59 @@ const Login = () => {
             </div>
           </div>
         </div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
       </div>
     </div>
+
   );
 };
 
