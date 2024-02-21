c1d006e4fe643e51fb6276d0145ad1659f8f4d56
Idelvalverde16 <idelvalverde16work@outlook.com>
2024-02-21 10:50:02 -0400

    agregando componentes

---
+++ front_end/src/Componentes/Comp_Helpers/Iconos.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+
+export const IconoXD = ({ width, height }) => {
+    return (
+        <>
+            <lord-icon
+                src="https://cdn.lordicon.com/kthelypq.json"
+                trigger="hover"
+                colors="primary:#ffffff"
+                style={{ width: `${width}px`, height: `${height}px` }}
+            >
+            </lord-icon>
+        </>
+    );
+};
+
+export const IconoHome = ({ width, height }) => {
+    return(
+        <>
+            <lord-icon
+               src="https://cdn.lordicon.com/cnpvyndp.json"
+               trigger="hover"
+               colors="primary:#ffffff"
+               style={{ width: `${width}px`, height: `${height}px` }} 
+            >
+            </lord-icon>
+        </>
+    )
+};
+
+export const IconoMultas = ({ width, height }) => {
+    return(
+        <>
+            <lord-icon
+               src="https://cdn.lordicon.com/jkzgajyr.json"
+               trigger="hover"
+               colors="primary:#ffffff"
+               style={{ width: `${width}px`, height: `${height}px` }} 
+            >
+            </lord-icon>
+        </>
+    )
+};
+
+export const IconoHistorial = ({ width, height }) => {
+    return(
+        <>
+            <lord-icon
+               src="https://cdn.lordicon.com/whrxobsb.json"
+               trigger="hover"
+               colors="primary:#ffffff"
+               style={{ width: `${width}px`, height: `${height}px` }} 
+            >
+            </lord-icon>
+        </>
+    )
+};
+
+export const Icono = ({ width, height }) => {
+    return (
+        <>
+            <lord-icon
+                src="https://cdn.lordicon.com/rmkahxvq.json"
+                trigger="hover"
+                colors="primary:#ffffff"
+                style={{
+                    width: `${width}px`,
+                    height: `${height}px`,
+                    position: 'relative',
+                    right: '12px',
+                    top: '-6px',
+                    transform: 'rotate(90deg)'
+                }}
+            >
+            </lord-icon>
+        </>
+    )
+};
+
+export const IconoTop = ({ width, height }) => {
+    return(
+        <>
+            <lord-icon
+               src="https://cdn.lordicon.com/dwoxxgps.json"
+               trigger="hover"
+               style={{ width: `${width}px`, height: `${height}px`  }} 
+            >
+            </lord-icon>
+        </>
+    )
+};
---
+++ front_end/src/Componentes/Home.jsx
@@ -0,0 +1,156 @@
+import React, { useState } from 'react';
+import "../Css/Footer-Dark-icons.css";
+import "../Css/sidebar-menu.css";
+import "../Css/bootstrap.min.css";
+import "../Css/animate.min.css";
+import "../Css/adicciones.css"
+import { IconoTop } from "../Componentes/Comp_Helpers/Iconos.jsx"
+import Footer from './Comp_Helpers/Footer.jsx';
+import Navbar from './Comp_Helpers/Navbar.jsx';
+import Topbar from './Comp_Helpers/Topbar.jsx';
+
+import toast, { Toaster } from 'react-hot-toast';
+
+
+function Map() {
+    return (
+        <div className="col-md-6">
+            <iframe allowFullScreen frameBorder="0" src="https://cdn.bootstrapstudio.io/placeholders/map.html" width="100%" height="400"></iframe>
+        </div>
+    );
+}
+
+function InformacionesCard() {
+    return (
+        <div className="col-md-6">
+            {/* Contenido de la tarjeta de Informaciones */}
+            <div className="card shadow mb-4">
+                <div className="card-header py-3" >
+                    <h6 className="text-success fw-bold m-0">Informaciones</h6>
+                </div>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <div className="row align-items-center no-gutters">
+                            <div className="col me-2">
+                                <h6 className="mb-0"><strong>Lunch meeting</strong></h6><span className="text-xs">10:30 AM</span>
+                            </div>
+                            <div className="col-auto">
+                                <div className="form-check"><input className="form-check-input" type="checkbox" id="formCheck-1"/><label className="form-check-label" htmlFor="formCheck-1"></label></div>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="list-group-item">
+                        <div className="row align-items-center no-gutters">
+                            <div className="col me-2">
+                                <h6 className="mb-0"><strong>Lunch meeting</strong></h6><span className="text-xs">11:30 AM</span>
+                            </div>
+                            <div className="col-auto">
+                                <div className="form-check"><input className="form-check-input" type="checkbox" id="formCheck-2"/><label className="form-check-label" htmlFor="formCheck-2"></label></div>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="list-group-item">
+                        <div className="row align-items-center no-gutters">
+                            <div className="col me-2">
+                                <h6 className="mb-0"><strong>Lunch meeting</strong></h6><span className="text-xs">12:30 AM</span>
+                            </div>
+                            <div className="col-auto">
+                                <div className="form-check"><input className="form-check-input" type="checkbox" id="formCheck-3"/><label className="form-check-label" htmlFor="formCheck-3"></label></div>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="list-group-item">
+                        <div className="row align-items-center no-gutters">
+                            <div className="col me-2">
+                                <h6 className="mb-0"><strong>Lunch meeting</strong></h6><span className="text-xs">12:30 AM</span>
+                            </div>
+                            <div className="col-auto">
+                                <div className="form-check"><input className="form-check-input" type="checkbox" id="formCheck-4"/><label className="form-check-label" htmlFor="formCheck-4"></label></div>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="list-group-item">
+                        <div className="row align-items-center no-gutters">
+                            <div className="col me-2">
+                                <h6 className="mb-0"><strong>Lunch meeting</strong></h6><span className="text-xs">12:30 AM</span>
+                            </div>
+                            <div className="col-auto">
+                                <div className="form-check"><input className="form-check-input" type="checkbox" id="formCheck-5"/><label className="form-check-label" htmlFor="formCheck-5"></label></div>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="list-group-item">
+                        <div className="row align-items-center no-gutters">
+                            <div className="col me-2">
+                                <h6 className="mb-0"><strong>Lunch meeting</strong></h6><span className="text-xs">12:30 AM</span>
+                            </div>
+                            <div className="col-auto">
+                                <div className="form-check"><input className="form-check-input" type="checkbox" id="formCheck-6"/><label className="form-check-label" htmlFor="formCheck-6"></label></div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+function MultasRecientesCard() {
+    return (
+        <div className="col">
+            {/* Contenido de la tarjeta de Multas Recientes */}
+            <div className="card shadow mb-4">
+                <div className="card-header py-3">
+                    <h6 className="text-success fw-bold m-0">Multas Recientes</h6>
+                </div>
+                <div className="card-body">
+                    <h4 className="small fw-bold">Customer Database<span className="float-end">60%</span></h4>
+                    <div className="progress mb-4">
+                        <div className="progress-bar bg-primary" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={{ width: '60%' }}><span className="visually-hidden">60%</span></div>
+                    </div>
+                    <h4 className="small fw-bold">Payout Details<span className="float-end">80%</span></h4>
+                    <div className="progress mb-4">
+                        <div className="progress-bar bg-info" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style={{ width: '80%' }}><span className="visually-hidden">80%</span></div>
+                    </div>
+                    <h4 className="small fw-bold">Account setup<span className="float-end">Complete!</span></h4>
+                    <div className="progress mb-4">
+                        <div className="progress-bar bg-success" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style={{ width: '100%' }}><span className="visually-hidden">100%</span></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+function Home() {
+
+    return (
+        <>
+        <div id="page-top"></div>
+        <div id="wrapper">
+            <Navbar />
+            <div className="d-flex flex-column" id="content-wrapper">
+                <Topbar titulo="Home"/>
+                <div className="container">
+                    <div className="row">
+                        <Map />
+                        <InformacionesCard />
+                    </div>
+                </div>
+                <div className="container-fluid">
+                    <div className="d-sm-flex justify-content-between align-items-center mb-4"></div>
+                    <div className="row">
+                        <MultasRecientesCard />
+                    </div>
+                </div>
+                <Footer />
+            </div>
+            <a className="border rounded d-inline scroll-to-top" href="#page-top"> <IconoTop width={40} height={40} /><i className="fas fa-angle-up"> </i></a>
+        </div>
+        </>
+    );
+}
+
+export default Home;
+
