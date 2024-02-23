db2633d40394374bc5d4d8e4e1b087d14df539d2
Angel Garcia <122624789+AngelGarciaCabrera@users.noreply.github.com>
2024-02-22 22:24:47 -0400

    Uso del localstorage

--- front_end/src/context/provider.jsx
+++ front_end/src/context/provider.jsx
@@ -3,14 +3,21 @@ import Context from "./context";
 
 
 export const Provider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem('user');
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
 
   const loginUser = (userData) => {
     setUser(userData);
+    localStorage.setItem('user', JSON.stringify(userData));
   };
+
   const logoutUser = () => {
     setUser(null);
+    localStorage.removeItem('user');
   };
+
 
 
    
