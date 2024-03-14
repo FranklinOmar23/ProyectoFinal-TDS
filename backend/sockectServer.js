0490e875fa3bd71e85e7a8cd16b2dd705caddeec
Franklin Disla <113482376+FranklinOmar23@users.noreply.github.com>
2024-03-14 08:28:53 -0400

    Update sockectServer.js

--- backend/sockectServer.js
+++ backend/sockectServer.js
@@ -16,7 +16,6 @@ io.on("connection", (socket) => {
 
     socket.on("message", (message) => {
         console.log(`Mensaje recibido de ${socket.id}: ${JSON.stringify(message)}`);
-        // Emitir el mensaje de vuelta al cliente
         io.emit("message", message);
         console.log(`Mensaje enviado a ${socket.id}: ${JSON.stringify(message)}`);
     });
